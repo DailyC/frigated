d2d757baa2b9e620efae4ddd4e9405c7ed6ab4bc
weiwei.wang <weiwei02@vip.qq.com>
2021-01-04 18:12:38 +0800

    repair retry logic of  start fail

--- pkgs/frigate/frigated_test.go
+++ pkgs/frigate/frigated_test.go
@@ -1,8 +1,13 @@
 package frigate
 
 import (
+	"context"
+	"io"
+	"os"
+	"os/exec"
 	"testing"
 	"time"
+	"unsafe"
 )
 
 func TestProtect(t *testing.T) {
@@ -27,10 +32,49 @@ func TestProtect(t *testing.T) {
 			}
 			time.Sleep(time.Second * 40)
 			tt.args.f.Stop(0)
-			time.Sleep(time.Second * 15)
 			if tt.args.f.ProtectTask.Process != nil {
 				t.Error("子进程无法正常结束")
 			}
 		})
 	}
+}
+
+// TestunsafePointModiffyPrivite 非安全的修改os.Cmd 内部变量
+func TestUnsafePointModiffyPrivite(t *testing.T) {
+	cmd := exec.Command("/usr/bin/top")
+	// cmd 开始指针
+	ps := unsafe.Pointer(cmd)
+	type AlignS struct {
+		ProcessState    *os.ProcessState
+		ctx             context.Context // nil means none
+		lookPathErr     error           // LookPath error, if any.
+		finished        bool            // when Wait was called
+		childFiles      []*os.File
+		closeAfterStart []io.Closer
+		closeAfterWait  []io.Closer
+		goroutine       []func() error
+		errch           chan error // one send per goroutine
+		waitDone        chan struct{}
+	}
+
+	as := &AlignS{}
+
+	t.Run("测试非安全修改结构体私有变量", func(t *testing.T) {
+		t.Logf("cmd start address is %d", uintptr(ps))
+		t.Logf("cmd size is %d", unsafe.Sizeof(*cmd))
+		t.Logf("cmd align size is %d", unsafe.Alignof(*cmd))
+		// t.Logf("cmd value attr offset addr is %d", unsafe.Offsetof((*cmd).ProcessState))
+
+		// 测试计算偏移量的占用内存大小关系
+		if unsafe.Offsetof(cmd.ProcessState) + unsafe.Sizeof(*as) != unsafe.Sizeof(*cmd) {
+			t.Logf("cmd attr ProcessState addr is %d", unsafe.Offsetof(cmd.ProcessState))
+			t.Logf("size of AlignS %d", unsafe.Sizeof(*as))
+			t.Errorf("state addr offset + size of AlignS is not eq size of cmd")
+		}
+
+		// alignF := unsafe.Offsetof(as.finished)
+		
+		
+		// t.Logf("size of AlignS %d", unsafe.Sizeof(as.SysProcAttr))
+	})
 }
